110310f2493b150f5036a77a59ed7116daff1c5c
Alexey Severin <alex.nixie@mail.ru>
2019-09-23 23:37:54 +0300

    end

--- ITMO_SPO_Task_1.sh
+++ ITMO_SPO_Task_1.sh
@@ -1,18 +1,45 @@
-FILES=/C/Users/AlexeySeverin/Desktop/Practika/*
-for f in $FILES
+#!/bin/bash
+
+# Ask the user for enter directory path
+# path example: /C/Users/AlexeySeverin/Desktop/Practika/*
+
+echo "Enter directory path (format /../...../*):"
+read path
+
+# Delete previous .xls file
+rm report.xls
+# Add top line in .xls file
+echo -e "NAME \t EXTENSION \t MODIFIED \t SIZE \t DURATION" >> report.xls
+# Publish information for any files in directory
+for f in $path
 do
-  filename="${f##*/}"
-  NAME=$(echo "$filename" | sed 's/\.[^.]*$//')
-    EXTENSION=$(echo "$filename" | sed 's/^.*\.//')
-    SIZE=$(wc -c "$f" | awk '{print $1}')
-  if [[ -d "$f" ]]; then
-   EXTENSION="dir"
-   SIZE="dir"
-  fi
-  DUR=$(
-ffprobe -v quiet -print_format compact=print_section=0:nokey=1:escape=csv -show_entries format=duration "$f")
+	# Delete text from the start line to the last match
+	filename="${f##*/}"
 
+	# Delete extention, for get name
+	NAME=$(echo "$filename" | sed 's/\.[^.]*$//')
+	echo $NAME
 
-  
-  echo -e "$NAME \t $EXTENSION \t $SIZE \t $DUR" >> myfile.xls
+	# Delete name, for get extention
+  	EXTENSION=$(echo "$filename" | sed 's/^.*\.//')
+	echo $EXTENSION
+
+	# Get file modified date
+	MODIFIED=$(stat -c "%y" "$f" | awk '{print $1}')
+	echo $MODIFIED
+
+	# Get file size
+  	SIZE=$(du -sh "$f" | awk '{print $1}')
+	echo $SIZE
+
+	if [[ -d "$f" ]]; then
+ 		EXTENSION="dir"
+ 		SIZE="dir"
+	fi
+
+	# Get video duration
+	DURATION=$(ffprobe -v quiet -print_format compact=print_section=0:nokey=1:escape=csv -show_entries format=duration "$f")
+	
+	# Write results to the report.xls
+	echo -e "$NAME \t $EXTENSION \t $MODIFIED \t $SIZE \t $DURATION" >> report.xls
 done
